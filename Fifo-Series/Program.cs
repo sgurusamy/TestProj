a10a6967fd6f45f2e66ab0e501d069901e362a3d
sankar gurusamy <sankar.gurusamy@gmail.com>
2015-10-16 18:33:14 -0600

    Initial code

---
+++ Fifo-Series/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fifo_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //New comments
+            Console.Write("Enter the length of the Fibonacci Series: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0} ", FibonacciSeries(i));
+            }
+            Console.ReadKey(); 
+        }
+
+        public static int FibonacciSeries(int n)
+        {
+            if (n == 0) return 0; //To return the first Fibonacci number   
+            if (n == 1) return 1; //To return the second Fibonacci number   
+            return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+        }  
+    }
+}
